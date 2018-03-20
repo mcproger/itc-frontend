7aeeceea7f9468b548db8e38629beb76182dbfd4
mcproger <mcproger7@gmail.com>
2018-03-20 12:35:24 +0300

    Add unpacking json response and generating post block

--- homework1/index.js
+++ homework1/index.js
@@ -1,21 +1,24 @@
 const redditApiUrl = 'https://www.reddit.com/top.json'
 
 
-const generatePostEntry = (imageSrc) => {
-  postBlock = `<div class="media"> \
-    <div class="media-left"> \
-      <img src="${imageSrc}" onerror="this.src=default.jpg" class="media-object" style="width:60px"> \
+const generatePostEntry = posts => {
+  data.data.children.map(post => {
+    postBlock = `<div class="media"> \
+      <div class="media-left"> \
+        <img src="${post.data.thumbnail}" onerror="this.src='default.jpg'" class="media-object" style="width:60px"> \
+      </div> \
+      <div class="media-body"> \
+        <h4 class="media-heading"></h4> \
+        <a href="${post.data.permalink}">${post.data.title}</p> \
+      </div> \
     </div> \
-    <div class="media-body"> \
-      <h4 class="media-heading">Left-aligned</h4> \
-      <a href="${postLink}">${postTitle}</p> \
-    </div> \
-  </div> \
-  `
-  document.getElementById('container').innerHTML += postBlock
+    `
+    document.getElementById('container').innerHTML += postBlock
+  })
 }
 
+
 fetch(redditApiUrl)
-  .then((response) => response.json())
-  .then(data => generatePostEntry(data))
+  .then(response => response.json())
+  .then(posts => generatePostEntry(posts))
   .catch(error => console.log(error));
